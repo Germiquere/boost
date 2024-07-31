49c2dba6ee1d01940d74be68bbe7337c8aff6695
Germiquere <german150703.miquere@gmail.com>
2024-07-31 18:15:48 -0300

    welcome page done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import {Link, NavLink } from "react-router-dom"
 import logoBlack from "../assets/home-headdecor-coffe1-black.png"
+import { Hero } from "./Hero";
 interface NavLinkClassProps {
     isActive: boolean;
     isPending: boolean;
@@ -8,9 +9,9 @@ interface NavLinkClassProps {
 export const Header = () => {
     // array with the links 
     const links =[
-        {name:"Bienvenidos",href:"/"},
-        {name:"Catalogo",href:"/catalogo"},
-        {name:"Contacto",href:"/contacto"},
+        {name:"INICIO",href:"/"},
+        {name:"CATALOGO",href:"/catalogo"},
+        {name:"CONTACTO",href:"/contacto"},
     ];
 
     // function to change the text color when the link is active based on the url
@@ -27,18 +28,18 @@ export const Header = () => {
     return (
         <header className="bg-white">
             <div className="mx-auto flex  justify-between items-center max-w-7xl w-full">
-            <Link to="/">
-            <img src={logoBlack} alt="logo"/>
-            </Link>
-            <nav className="flex items-center justify-center p-6 lg:px-8 gap-3 text-sm sm:text-base" aria-label="Global">
-                {
-                    links.map( link => (
-                        <NavLink key={link.name} to={link.href} className={getNavLinkClass}>{link.name}</NavLink>
-                    ))
-                }
-            </nav>
+                <Link to="/">
+                <img src={logoBlack} alt="logo"/>
+                </Link>
+                <nav className="flex items-center justify-center p-6 lg:px-8 gap-3 text-sm sm:text-base font-NotoSants" aria-label="Global">
+                    {
+                        links.map( link => (
+                            <NavLink key={link.name} to={link.href} className={getNavLinkClass}>{link.name}</NavLink>
+                        ))
+                    }
+                </nav>
             </div>
-            
+            <Hero/>
         </header>
     )
 }
---
+++ src/components/ProductFact.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Fact } from "./ProductFacts";
+
+interface FactItemProps {
+    fact: Fact;
+}
+
+export const ProductFact: React.FC<FactItemProps> = ({fact}) => {
+    const {img,text,title} = fact
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-5 relative">
+            <div className="rounded-full bg-white border-green-600 w-44 h-44 border-4 flex justify-center items-center text-green-600">
+                {img}
+            </div>
+            <h3 className="font-semibold text-lg font-NotoSants">{title}</h3>
+            <p className="w-56 font-NotoSants text-center">{text}</p>
+        </div>
+    )
+}
